bf8085584d8cd366d55ae903a8b4d04648c1681f
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-18 21:28:40 +0530

    Added : Program for To find Maximum number

--- src/test/java/com/bridgelabz/GenericsPrograms/MaximumTest.java
+++ src/test/java/com/bridgelabz/GenericsPrograms/MaximumTest.java
@@ -1,5 +1,26 @@
 package com.bridgelabz.GenericsPrograms;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class MaximumTest {
+	@Test
+	public void checkmaximum1() {
+		int max = Maximum.getmaximum(9, 4, 1);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum2() {
+		int max = Maximum.getmaximum(1, 9, 4);
+		Assert.assertEquals(9, max);
+	}
+
+	@Test
+	public void checkmaximum3() {
+		int max = Maximum.getmaximum(1, 4, 9);
+		Assert.assertEquals(9, max);
+	}
 }
 
