d55b20d861158f5be0ab54328a14ab461157a8d2
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-18 22:20:10 +0530

    Added : program for the multiple element

--- src/test/java/com/bridgelabz/GenericsPrograms/MaximumTest.java
+++ src/test/java/com/bridgelabz/GenericsPrograms/MaximumTest.java
@@ -5,27 +5,26 @@ import org.junit.Test;
 import junit.framework.Assert;
 
 public class MaximumTest {
-	@Test
-	public void intMax() {
-		int actualValue = Maximum.testMaximum(9, 4, 1);
-		Assert.assertEquals(9, actualValue);
-	}
+	 @Test
+	    public void intMax() {
+	        int actualValue = Maximum.testMaximum(9, 4, 1);
+	        Assert.assertEquals(9, actualValue);
+	    }
 
-	@Test
-	public void floatMax() {
-		float actualValue = Maximum.testMaximum(1.5f, 9.1f, 4.4f);
-		Assert.assertEquals(9.1f, actualValue, 0);
-	}
+	    @Test
+	    public void floatMax() {
+	        float actualValue = Maximum.testMaximum(1.5f,9.1f,4.4f);
+	        Assert.assertEquals(9.1f,actualValue,0);
+	    }
 
-	@Test
-	public void stringMax() {
-		String actualValue = Maximum.testMaximum("apple", "peach", "banana");
-		Assert.assertEquals("peach", actualValue);
-	}
-
-	@Test
-	public void givenStringValues_WhenGetMaximum_ShouldReturnCorrectValue() {
-		String actualResult = Maximum.testMaximum("Apple", "Peach", "Banana");
-		Assert.assertEquals("Peach", actualResult);
-	}
+	    @Test
+	    public void stringMax() {
+	        String actualValue = Maximum.testMaximum("apple","peach","banana");
+	        Assert.assertEquals("peach",actualValue);
+	    }
+	    @Test
+	    public void givenStringValues_WhenGetMaximum_ShouldReturnCorrectValue() {
+	        String actualResult = Maximum.testMaximum("Apple", "Peach", "Banana", "Mango","Strawberry");
+	        Assert.assertEquals("Strawberry", actualResult);
+	    }
 }
